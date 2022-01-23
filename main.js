24b7c3737e98a2b60711e9097efa7d210f450737
Abuzar Haider <abuzarsarwary3@gmail.com>
2022-01-24 00:04:18 +0500

    Initial Commit

---
+++ main.js
@@ -0,0 +1,57 @@
+// 3 Methods to print
+/*
+    > console.log();        to write in console
+    > alert();      to write in dialogue box
+    > document.write();     to write in browser window */
+
+// function display(){
+//     document.write("Thanks for clicking")
+// }
+// document.querySelector('#divName').innerHTML = "Hello! How are you?"; 
+
+// ---------------
+
+/*
+    > Combining a num+num = num
+    > Combining a string+ string = string
+    > Combining a String + num = String */
+
+    // var hello = "My name is Aslam";
+    // var age = 26;
+    // var mix = "I am  living in NewYork for " + 10 + " years"; 
+    // document.querySelector('#divName').innerHTML = hello + "<br>" + age + "<br>" + mix;
+
+/*
+Boolean Data Type
+    > It returns TRUE or FALSE values
+    > Also used in conditional/comparison operators "=="    */
+
+    // var num1 = 4; 
+    // var num2 = 3;
+    // var equal = num1==num2;
+    // document.querySelector('#divName').innerHTML = equal;
+
+/*
+Object Data Type
+    > Collection of related data
+    > Contain properties written in key value pairs. Each pair is separated by a comma(,)
+    > Objects are written inside curly braces   */
+
+    // var fruit = {
+    //     apple: "red",
+    //     guava: "green",
+    //     banana: "yellow",
+    //     isSweet: true,
+    // }
+    // document.querySelector('#divName').innerHTML = "Color of Apple is " + fruit.apple + "<br> Color of guava is " + fruit.guava + "<br> Color of Banana " + fruit.banana + "<br> Are these fruits Sweet?  " + fruit.isSweet ;
+
+/*
+Arrays
+    > It is a sequential collection of values
+    > Values are written inside a square brackets [] and separated by comma(,)
+    > Data Type of array is object  */
+
+    // var Name = "John Titor";
+    // var Age = 38;
+    // var array = ["Hello!", Name, "You are " + Age + " years old."];
+    // document.querySelector('#divName').innerHTML = array[0] + " " + array[1] + " " + array[2];
